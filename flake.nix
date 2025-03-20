2408bcaca15512d04e7c392e294252e432ce6e3d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-21 00:18:47 +0200

    feat: create flake.nix

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "A collection of Nix flake templates I personally use";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { ... }: {
+  };
+}
